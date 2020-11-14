b0db170be0248647a95e5b31bf77431ad6638e09
Nelson Wells <nelsonwellswku@gmail.com>
2020-11-14 15:15:44 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace docker_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Double rainbow");
+            Console.Read();
+        }
+    }
+}
